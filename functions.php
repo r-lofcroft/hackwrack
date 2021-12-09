10492fb59609d8ee44d35938dfe958f4c25bcda7
Robin Williscroft <robin.williscroft@gmail.com>
2021-12-09 12:34:09 +0100

    bug-fixed variable declaration post merge. added styling for landing page

--- functions.php
+++ functions.php
@@ -20,7 +20,17 @@ function hackwrackMenus()
   register_nav_menus($locations);
 }
 add_action('init', 'hackwrackMenus');
-
+/**
+ * Filter the excerpt length to 30 words.
+ *
+ * @param int $length Excerpt length.
+ * @return int (Maybe) modified excerpt length.
+ */
+function wp_example_excerpt_length($length)
+{
+  return 30;
+}
+add_filter('excerpt_length', 'wp_example_excerpt_length');
 
 function hackwrackRegisterStyles()
 {
@@ -68,78 +78,81 @@ function hackwrackWidgetAreas()
     )
   ); */
 
-  register_sidebar( array(
-'name' => 'Footer Area 1',
-'id' => 'footer-1',
-'description' => 'Appears in the footer area',
-'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-'after_widget' => '</aside>',
-'before_title' => '<h3 class="widget-title">',
-'after_title' => '</h3>',
-) );
+  register_sidebar(array(
+    'name' => 'Footer Area 1',
+    'id' => 'footer-1',
+    'description' => 'Appears in the footer area',
+    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+    'after_widget' => '</aside>',
+    'before_title' => '<h3 class="widget-title">',
+    'after_title' => '</h3>',
+  ));
 
-register_sidebar( array(
-'name' => 'Footer Area 2',
-'id' => 'footer-2',
-'description' => 'Appears in the footer area',
-'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-'after_widget' => '</aside>',
-'before_title' => '<h3 class="widget-title">',
-'after_title' => '</h3>',
-) );
+  register_sidebar(array(
+    'name' => 'Footer Area 2',
+    'id' => 'footer-2',
+    'description' => 'Appears in the footer area',
+    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+    'after_widget' => '</aside>',
+    'before_title' => '<h3 class="widget-title">',
+    'after_title' => '</h3>',
+  ));
 
-register_sidebar( array(
-'name' => 'Footer Area 3',
-'id' => 'footer-3',
-'description' => 'Appears in the footer area',
-'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-'after_widget' => '</aside>',
-'before_title' => '<h3 class="widget-title">',
-'after_title' => '</h3>',
-) );
-
+  register_sidebar(array(
+    'name' => 'Footer Area 3',
+    'id' => 'footer-3',
+    'description' => 'Appears in the footer area',
+    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+    'after_widget' => '</aside>',
+    'before_title' => '<h3 class="widget-title">',
+    'after_title' => '</h3>',
+  ));
 }
 add_action('widgets_init', 'hackwrackWidgetAreas');
 
-function list_cases() {
-  $query = new WP_Query( array( 
-      'category_name' => 'Cases', 
-      'posts_per_page' => 4
-  ) ); 
-  if ( $query->have_posts() ) {
+function list_cases()
+{
+  global $string;
+  $query = new WP_Query(array(
+    'category_name' => 'Cases',
+    'posts_per_page' => 4
+  ));
+  if ($query->have_posts()) {
     $string .= '<div class="customers-items">';
-    while ( $query->have_posts() ) {
+    while ($query->have_posts()) {
       $query->the_post();
-      $string .= '<div class="customer-item"><a href="'.get_the_permalink().'">';
+      $string .= '<div class="customer-item"><a href="' . get_the_permalink() . '">';
       $string .= '<div class="customer-item-img">';
-      $string .= '<img src="'.get_field('case_image').'" alt="Customer" width="200px">';
+      $string .= '<img src="' . get_field('case_image') . '" alt="Customer" width="200px">';
       $string .= '</div>';
-      $string .= '<div class="customer-item-desc"><h3>'. get_field('case_name') .'</h3><p>'.get_field('case_desc').'</p></div>';
+      $string .= '<div class="customer-item-desc"><h3>' . get_field('case_name') . '</h3><p>' . get_field('case_desc') . '</p></div>';
       $string .= '</a></div>';
     }
-  } 
+  }
   $string .= '</div>';
   return $string;
   wp_reset_postdata();
 }
 add_shortcode('show_cases', 'list_cases');
 
-function list_stories() {
-  $query = new WP_Query( array( 
-      'category_name' => 'Stories', 
-      'posts_per_page' => 2
-  ) ); 
-  if ( $query->have_posts() ) {
-    $string .= '<div class="stories-items">';
-    while ( $query->have_posts() ) {
-    $query->the_post();
-      $string .= '<div class="stories-item" style="background: linear-gradient(0deg, rgba(250,250,250,1) 10%, rgba(255,255,255,0) 100%), url('.get_field('story_image').')"><a href="'.get_the_permalink().'">';
-      $string .= '<div class="stories-item-desc"><h3>' . get_field('story_name') .'</h3><p>'.get_field('story_desc').'</p></div>';
-      $string .= '</a></div>';
+function list_stories()
+{
+  global $stringStory;
+  $query = new WP_Query(array(
+    'category_name' => 'Stories',
+    'posts_per_page' => 2
+  ));
+  if ($query->have_posts()) {
+    $stringStory .= '<div class="stories-items">';
+    while ($query->have_posts()) {
+      $query->the_post();
+      $stringStory .= '<div class="stories-item" style="background: linear-gradient(0deg, rgba(250,250,250,1) 10%, rgba(255,255,255,0) 100%), url(' . get_field('story_image') . ')"><a href="' . get_the_permalink() . '">';
+      $stringStory .= '<div class="stories-item-desc"><h3>' . get_field('story_name') . '</h3><p>' . get_field('story_desc') . '</p></div>';
+      $stringStory .= '</a></div>';
     }
-  } 
-  $string .= '</div>';
-  return $string;
+  }
+  $stringStory .= '</div>';
+  return $stringStory;
   wp_reset_postdata();
 }
 add_shortcode('show_stories', 'list_stories');
